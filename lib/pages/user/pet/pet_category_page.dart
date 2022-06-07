746b203f1b9e4d61aca56b4bab639e773fb9c4e8
dingwenkai <dingwenkai@huami.com>
2022-06-08 00:21:03 +0800

    feat |> 优化分类选择界面视觉

--- lib/pages/user/pet/pet_category_page.dart
+++ lib/pages/user/pet/pet_category_page.dart
@@ -17,27 +17,36 @@ class PetCategoryPage extends GetView<PetCategoryController>
         appBar: AppBar(title: const Text('选择分类')),
         body: SafeArea(
             child: GetBuilder<PetCategoryController>(
-                builder: (_) => GridView.builder(
-                    itemCount: controller.categories.length,
-                    itemBuilder: _gridContent,
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisSpacing: 10.0,
-                            mainAxisSpacing: 10.0,
-                            crossAxisCount: 2)))));
+                builder: (_) => Padding(
+                    padding:
+                        const EdgeInsets.only(top: 20, left: 40, right: 40),
+                    child: GridView.builder(
+                        itemCount: controller.categories.length,
+                        itemBuilder: _gridContent,
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisSpacing: 10.0,
+                                mainAxisSpacing: 10.0,
+                                crossAxisCount: 2))))));
   }
 
-  Widget _gridContent(context, index) => GestureDetector(
-      onTap: () => controller.jumpToSub(index),
-      child: Container(
-        decoration:
-            BoxDecoration(border: Border.all(width: 1, color: kBorderColor)),
-        child: Column(children: [
-          controller.loadImage(index) == null
-              ? const Icon(Icons.local_laundry_service)
-              : buildNetImage(controller.loadImage(index)!),
+  Widget _gridContent(context, index) => Column(
+        children: [
+          SizedBox(
+              height: 100,
+              width: 100,
+              child: ClipOval(child: _imageView(index))),
           const SizedBox(height: 12),
           Text(controller.categories[index].name)
-        ]),
-      ));
+        ],
+      );
+
+  Widget _imageView(int index) => GestureDetector(
+      child: controller.loadImage(index) == null
+          ? Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Icon(Icons.camera_alt_rounded,
+                  size: 40, color: kSecondaryTextColor))
+          : buildNetImage(controller.loadImage(index)!, fit: BoxFit.fill),
+      onTap: () => controller.jumpToSub(index));
 }
