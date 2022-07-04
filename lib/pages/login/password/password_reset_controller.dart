b2a4cf12ea94c985a3b7e932133f04235172e98a
dingwenkai <dingwenkai@huami.com>
2022-07-04 22:47:38 +0800

    feat |> 初步添加密码设置页面

---
+++ lib/pages/login/password/password_reset_controller.dart
@@ -0,0 +1,79 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:frontend/components/mixins/validator.dart';
+import 'package:frontend/models/user/user.dart';
+import 'package:frontend/net/net_mixin.dart';
+import 'package:frontend/route/pages.dart';
+import 'package:frontend/services/launch_service.dart';
+import 'package:get/get.dart';
+import 'package:more/more.dart';
+
+class PasswordResetController extends GetxController with NetMixin {
+  final phoneCtl = TextEditingController();
+  final codeCtl = TextEditingController();
+  final pwdCtl = TextEditingController();
+
+  /// 是否选择同意用户协议
+  var selectedClause = true.obs;
+
+  /// 是否可登录
+  var loginEnable = false.obs;
+
+  /// 是否可获取验证码
+  var codeEnable = false.obs;
+
+  /// 验证码倒计时
+  var codeCounter = '获取验证码'.obs;
+
+  /// 验证码定时器
+  Timer? timer;
+
+  bool get shouldCodeLogin =>
+      codeCtl.text.length == 6 &&
+          Validator.phone.verify(phoneCtl.text) &&
+          selectedClause.value;
+
+  bool get shouldPwdLogin =>
+      pwdCtl.text.length >= 6 &&
+          Validator.phone.verify(phoneCtl.text) &&
+          selectedClause.value;
+
+  bool get shouldFetchCode =>
+      Validator.phone.verify(phoneCtl.text) && !(timer?.isActive ?? false);
+
+  fetchCode() {
+    timer?.cancel();
+    codeEnable.value = false;
+    timer = Timer.periodic(const Duration(seconds: 1), (t) {
+      if (t.tick == 10) {
+        codeCounter.value = '获取验证码';
+        timer?.cancel();
+        codeEnable.value = true;
+        return;
+      }
+      codeCounter.value = '${60 - t.tick}s';
+    });
+  }
+
+  login() {
+    success(Tuple2<String, User> result) {
+      LaunchService.shared.login(result.second, result.first);
+      final next = LaunchService.shared.isCompletedRegisterFlow;
+      next != null ? Get.toNamed(next) : Get.offAllNamed(AppRoutes.scaffold);
+    }
+    request<Tuple2<String, User>>(
+        api: () =>
+            post('user/login/', {'phone': phoneCtl.text, 'code': codeCtl.text},
+                    (data) {
+                  final token = data['token'];
+                  final user = User.fromJson(data['user']);
+                  return Tuple2(token, user);
+                }),
+        success: success);
+  }
+
+  loginWithAppleId() {}
+
+  loginWithWeChat() {}
+}
